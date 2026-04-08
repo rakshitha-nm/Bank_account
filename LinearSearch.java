429cee70cd06d38b0934d61e9e68a663c1770b47
Rakshitha N M <rakshitha9870@gmail.com>
2026-04-08 22:15:33 +0530

    Create LinearSearch.java

---
+++ LinearSearch.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class LinearSearch {
+
+    public int search(int arr[], int key) {
+
+        for(int i = 0; i < arr.length; i++) {
+
+            if(arr[i] == key) {
+                return i;
+            }
+
+        }
+
+        return -1;
+    }
+}
