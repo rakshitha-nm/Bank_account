65b262d96ec1b2817d752a5b8d573ef6abd2a252
Rakshitha N M <rakshitha9870@gmail.com>
2026-04-22 15:20:10 +0530

    Update CalculatorTest.java

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,29 +1,33 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class CalculatorTest {
 
     Calculator calc = new Calculator();
 
     @Test
-    void testAdd() {
+    public void testAdd() {
         assertEquals(5, calc.add(2,3));
     }
 
     @Test
-    void testSubtract() {
-        assertEquals(1, calc.subtract(3,2));
+    public void testSubtract() {
+        assertEquals(2, calc.subtract(5,3));
     }
 
     @Test
-    void testMultiply() {
-        assertEquals(6, calc.multiply(2,3));
+    public void testMultiply() {
+        assertEquals(6, calc.multiply(5,3));
     }
 
     @Test
-    void testDivide() {
+    public void testDivide() {
         assertEquals(2, calc.divide(6,3));
+    }
+    @Test(expected = ArithmeticException.class)
+    public void testDivideByZero() {
+        calc.divide(5,0);
     }
 }
